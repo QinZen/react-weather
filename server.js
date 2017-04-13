abb2852dbd80891d29081ab1f281a5d80b07469f
c26842684 <d26842684@hotmail.com>
2017-04-12 20:44:23 -0400

    Ready for deploy

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
+app.use(function (req,res,next){
+  if(req.headers['x-forward-proto']==='http'){
+    next();
+  }else{
+    res.redirect("http://" + req.hostname + req.url);
+  }
+});
+
+
 app.use(express.static("public"));
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
   console.log("server start up!");
 })
